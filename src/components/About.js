14b66c8cab2f218bf6b12831671d58dc30c84994
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-27 22:19:55 -0400

    react-hooks-props-basics-lab/
    react-hooks-props-basics-lab/

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Links from "./links"; // Update the import statement
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
-    </div>
+      <Links github={props.github} linkedin={props.linkedin} />
+    </div> // Add the closing tag for the <div> element
   );
 }
 
